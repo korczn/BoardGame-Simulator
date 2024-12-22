a28e9d4a4e9c8bed5c436ce3e3e8c26527ca4679
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-22 22:10:23 +0100

    Dodaj główną logikę programu do inicjowania i uruchamiania gry

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+namespace BoardGameSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Warrior("Warrior", 0);
+            Player player2 = new Mage("Mage", 0);
+            Player player3 = new Healer("Healer", 0);
+
+            Player[] players = new Player[] { player1, player2, player3 };
+            Board board = new Board(20);
+            Game game = new Game(players, board);
+
+            game.StartGame();
+
+            for (int i = 0; i < 5; i++)
+            {
+                game.PlayTurn();
+            }
+
+            game.DisplayScores();
+        }
+    }
+}
